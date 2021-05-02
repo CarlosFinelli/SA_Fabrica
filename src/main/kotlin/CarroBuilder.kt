f110789cc0e0415d715e43307d24ec3b4f7ca70e
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2021-05-02 18:34:17 -0300

    Conclusão da parte de manipulação dos clientes e funcionários, adicionando try/catch em todas as opções disponíveis dentro do enlace de repetição, além de criação das rotas para criação, edição e exclusão dos veículos, além da listagem de todos os veículos cadastrados

--- src/main/kotlin/CarroBuilder.kt
+++ src/main/kotlin/CarroBuilder.kt
@@ -1,8 +1,3 @@
 abstract open class CarroBuilder {
-    abstract fun buildPreco(preco: Double)
-    abstract fun buildDcsMotor(dscMotor: String)
-    abstract fun buildAnoDeFabricacao(anoFabricacao: Integer)
-    abstract fun buildModelo(modelo: String)
-    abstract fun buildMontadora(montadora: String)
-    abstract fun getCarro(): CarroProduct
+
 }
--- src/main/kotlin/CarroProduct.kt
+++ src/main/kotlin/CarroProduct.kt
@@ -2,31 +2,9 @@ class CarroProduct: CarroBuilder() {
 
     var preco: Double = 0.0
     lateinit var dscMotor: String
-    lateinit var anoDeFabricacao: Integer
+    var anoDeFabricacao: Int = 0
     lateinit var modelo: String
     lateinit var montadora: String
-
-    override fun buildPreco(preco: Double) {
-        this.preco = preco
-    }
-
-    override fun buildDcsMotor(dscMotor: String) {
-        this.dscMotor = dscMotor
-    }
-
-    override fun buildAnoDeFabricacao(anoFabricacao: Integer) {
-        this.anoDeFabricacao = anoFabricacao
-    }
-
-    override fun buildModelo(modelo: String) {
-        this. modelo = modelo
-    }
-
-    override fun buildMontadora(montadora: String) {
-        this.montadora = montadora
-    }
-
-    override fun getCarro(): CarroProduct {
-        return this
-    }
+    var codVeiculo: Int = 0
+    var exibicao = false
 }
